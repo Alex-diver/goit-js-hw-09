b6a41fa39576ed5f9704176d6325d608be2d418b
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-06-22 18:02:33 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalid;
+
+function startColorSwitching() {
+  startBtn.disabled = true;
+
+  intervalid = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopColorSwitchihg() {
+  startBtn.disabled = false;
+  clearInterval(intervalid);
+}
+
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitchihg);
